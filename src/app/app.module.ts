d63083e73ccd330cb343c6a58b940d7fc45b53e5
guanzhanliang <19879753@qq.com>
2017-07-18 16:59:50 +0800

    contact.service.ts的 const CONTACT_URL = 'app/contacts.json'; 修改成单引号

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import {NgModule} from '@angular/core'
-import {RouterModule} from "@angular/router";
-import {FormsModule} from "@angular/forms";
-import {BrowserModule} from "@angular/platform-browser";
-import {HttpModule} from "@angular/http";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import {rootRouterConfig} from "./app.routes";
 import {AppComponent} from "./app.component";
@@ -12,24 +12,26 @@ import {ListComponent, ListItemComponent} from './list';
 import {DetailComponent} from './detail';
 import {EditComponent} from './edit';
 
-import {ContactService, UtilService, FooterComponent, 
-  HeaderComponent, PhonePipe, BtnClickDirective} from "./shared";
+import {
+    ContactService, UtilService, FooterComponent,
+    HeaderComponent, PhonePipe, BtnClickDirective
+} from "./shared";
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ListComponent, ListItemComponent,
-    DetailComponent,
-    CollectionComponent,
-    EditComponent,
-    HeaderComponent, FooterComponent,
-    PhonePipe,
-    BtnClickDirective
-  ],
-  imports     : [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
-  providers   : [ContactService, UtilService],
-  bootstrap   : [AppComponent]
+    declarations: [
+        AppComponent,
+        ListComponent, ListItemComponent,
+        DetailComponent,
+        CollectionComponent,
+        EditComponent,
+        HeaderComponent, FooterComponent,
+        PhonePipe,
+        BtnClickDirective
+    ],
+    imports: [BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(rootRouterConfig)],
+    providers: [ContactService, UtilService],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 
